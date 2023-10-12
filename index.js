04d04392395ac017388eb2161c9625326a23a8cf
Мирзазаде Эмиль Алибей <Mirza_bs61@itstep.edu.az>
2023-10-12 21:30:52 +0400

    5rd point from homework is done

--- index.js
+++ index.js
@@ -6,20 +6,47 @@ let regName=prompt("Enter the name: ");
 let regPass=prompt("Enter the password: ");
 let cnt=3;
 let reReg=false;
+let rePass=false;
 alert('You should log in.');
-while(true){
+outerLoop: while(true){
     let logName=prompt("Enter the name: ");
     let logPass=prompt("Enter the password: ");
-    if(logName===regName || logPass===regPass) break;
+    let rightName=logName===regName;
+    let rightPass=logPass===regPass;
+    if(rightName && rightPass) {
+        console.log(logName, regName);
+        console.log(logPass, regPass);
+        break;
+    }
     if(!cnt--){
         reReg=confirm("Would you like to register again?");
         cnt=3;
+        while(true){
+            if(reReg){
+                regName=prompt("Enter the name: ");
+                regPass=prompt("Enter the password: ");
+                continue outerLoop;
+            }
+            else{
+                rePass=confirm("Would you like to change password again?");
+            }
+            if(rePass){
+                logName=prompt("Enter the name: ");
+                rightName=logName===regName;
+                if(rightName){
+                    regPass=prompt("Enter the password: ");
+                    continue outerLoop;
+                }
+                reReg=confirm("Would you like to register again?");
+                if(reReg){
+                    continue;
+                }
+                continue outerLoop;
+            }
+        }
+
     }
-    if(reReg){
-        regName=prompt("Enter the name: ");
-        regPass=prompt("Enter the password: ");
-        continue;
-    }
+
     alert("Name or password were invalid. Try to login again.");
 }
 
