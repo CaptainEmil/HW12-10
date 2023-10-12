a6742f08a7a62c906689a82f4300c1a5d2614905
Мирзазаде Эмиль Алибей <Mirza_bs61@itstep.edu.az>
2023-10-12 19:43:57 +0400

    4th point of homework is done

--- index.js
+++ index.js
@@ -4,13 +4,22 @@ console.clear();
 alert('You should register.');
 let regName=prompt("Enter the name: ");
 let regPass=prompt("Enter the password: ");
-
-
+let cnt=3;
+let reReg=false;
 alert('You should log in.');
 while(true){
     let logName=prompt("Enter the name: ");
     let logPass=prompt("Enter the password: ");
     if(logName===regName || logPass===regPass) break;
+    if(!cnt--){
+        reReg=confirm("Would you like to register again?");
+        cnt=3;
+    }
+    if(reReg){
+        regName=prompt("Enter the name: ");
+        regPass=prompt("Enter the password: ");
+        continue;
+    }
     alert("Name or password were invalid. Try to login again.");
 }
 
